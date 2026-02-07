e11c39b313df5fbe02847f48b4cfd77e7463dfe7
Arvind kumar <arvind81493@gmail.com>
2026-02-07 14:16:54 +0530

    contructor in js

---
+++ Object/constructor.js
@@ -0,0 +1,15 @@
+function Employee(name ,age,colony){
+    this.name = name;
+    this.age = age;
+    
+
+    this.address={
+        street,
+        colony
+    }
+    
+}
+
+let emp1=new Employee("Bisag", 22, "Gandhinagar");
+console.log(emp1.name);
+console.log(emp1.address.colony);
