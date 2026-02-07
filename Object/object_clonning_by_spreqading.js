4bf05098c318f6a8764679869bf32205693a6b3b
Arvind kumar <arvind81493@gmail.com>
2026-02-07 15:09:52 +0530

    object clonning  by spreading in js

---
+++ Object/object_clonning_by_spreqading.js
@@ -0,0 +1,18 @@
+const rectangle = {
+    length: 2,
+    breadth: 4,
+
+    draw : function(){
+        console.log("Drawing rectangle");
+    },
+
+    area : function()
+    {
+        console.log(`Area of Rectangle: ${rectangle.length * rectangle.breadth}`);
+    }
+}
+
+
+const duplicate={...rectangle};
+
+console.log(duplicate);
